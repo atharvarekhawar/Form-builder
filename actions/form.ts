5684029b50aa012ce9a81f6aecee4e72f07c5be6
Atharva Rekhawar <atharvarekhawar64@gmail.com>
2023-10-13 12:58:16 +0530

    createForm btn added with prisma functionality

--- actions/form.ts
+++ actions/form.ts
@@ -2,6 +2,7 @@
 
 import prisma from "@/lib/prisma";
 import { currentUser } from "@clerk/nextjs"
+import { formSchema, formSchemaType } from "@/schemas/form";
 
 class UserNotFoundErr extends Error { }
 
@@ -35,4 +36,48 @@ export async function GetFormStats() {
     return {
         visits, submissions, submissionRate, bounceRate
     }
+}
+
+export async function CreateForm(data: formSchemaType) {
+    const validation = formSchema.safeParse(data);
+    if (!validation.success) {
+        throw new Error("form not valid");
+    }
+
+    const user = await currentUser();
+    if (!user) {
+        throw new UserNotFoundErr();
+    }
+
+    const { name, description } = data;
+
+    const form = await prisma.form.create({
+        data: {
+            userId: user.id,
+            name,
+            description,
+        },
+    });
+
+    if (!form) {
+        throw new Error("something went wrong");
+    }
+
+    return form.id;
+}
+
+export async function GetForms() {
+    const user = await currentUser();
+    if (!user) {
+        throw new UserNotFoundErr();
+    }
+
+    return await prisma.form.findMany({
+        where: {
+            userId: user.id,
+        },
+        orderBy: {
+            createdAt: "desc",
+        },
+    });
 }
