b1fc654031882684dfcfe959dc0b1748ce71587a
Atharva Rekhawar <atharvarekhawar64@gmail.com>
2023-10-15 23:01:38 +0530

    Added Designer Context and Designer d&d

--- components/Designer.tsx
+++ components/Designer.tsx
@@ -1,16 +1,50 @@
 "use client";
-import React from "react";
+import React, { useState } from "react";
 import DesignerSideBar from "./DesignerSideBar";
-import { useDroppable } from "@dnd-kit/core";
+import { DragEndEvent, useDndMonitor, useDroppable } from "@dnd-kit/core";
 import { cn } from "@/lib/utils";
+import {
+  ElementsType,
+  FormElementInstance,
+  FormElements,
+} from "./FormElements";
+import idGenerator from "@/lib/idGenerator";
+import useDesigner from "./hooks/useDesigner";
 
 const Designer = () => {
+  const { elements, addElement } = useDesigner();
   const droppable = useDroppable({
     id: "designer-drop-area",
     data: {
       isDesignerDropArea: true,
     },
   });
+
+  useDndMonitor({
+    onDragEnd: (event: DragEndEvent) => {
+      const { active, over } = event;
+      if (!active || !over) return;
+
+      const isDesignerBtnElement = active.data?.current?.isDesignerBtnElement;
+      const isDroppingOverDesignerDropArea =
+        over.data?.current?.isDesignerDropArea;
+
+      const droppingSidebarBtnOverDesignerDropArea =
+        isDesignerBtnElement && isDroppingOverDesignerDropArea;
+
+      // First scenario
+      if (droppingSidebarBtnOverDesignerDropArea) {
+        const type = active.data?.current?.type;
+        const newElement = FormElements[type as ElementsType].construct(
+          idGenerator()
+        );
+
+        addElement(elements.length, newElement);
+        return;
+      }
+    },
+  });
+
   return (
     <div className="flex w-full h-full">
       <div className="p-4 w-full">
@@ -21,7 +55,7 @@ const Designer = () => {
             droppable.isOver && "ring-4 ring-primary ring-inset"
           )}
         >
-          {!droppable.isOver && (
+          {!droppable.isOver && elements.length === 0 && (
             <p className="text-3xl text-muted-foreground flex flex-grow items-center font-bold">
               Drop here
             </p>
@@ -31,11 +65,24 @@ const Designer = () => {
               <div className="h-[120px] rounded-md bg-primary/20"></div>
             </div>
           )}
+          {elements.length > 0 && (
+            <div className="flex flex-col  w-full gap-2 p-4">
+            {elements.map((element) => (
+              <DesignerElementWrapper key={element.id} element={element} />
+            ))}
+          </div>
+          )}
         </div>
       </div>
       <DesignerSideBar />
     </div>
   );
 };
+
+const DesignerElementWrapper = ({element}:{element:FormElementInstance})=>{
+  const DesignerElement = FormElements[element.type].designerComponent;
+
+  return <DesignerElement elementInstance={element}/>
+}
 
 export default Designer;
