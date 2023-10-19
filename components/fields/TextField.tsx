9cac4ca8d9a32ce50c28c63a3366fabcbd0ea09b
Atharva Rekhawar <atharvarekhawar64@gmail.com>
2023-10-19 18:33:30 +0530

    Added preview button

--- components/fields/TextField.tsx
+++ components/fields/TextField.tsx
@@ -42,7 +42,7 @@ export const TextFieldFormElement: FormElement = {
     label: "Text Field",
   },
   designerComponent: DesignerComponent,
-  formComponent: () => <div>Designer component</div>,
+  formComponent: formComponent,
   propertiesComponent: PropertiesComponent,
 };
 
@@ -64,6 +64,27 @@ function DesignerComponent({
         {required && "*"}
       </Label>
       <Input readOnly disabled placeholder={placeHolder} />
+      {helperText && (
+        <p className="text-muted-foreground text-[0.8rem]">{helperText}</p>
+      )}
+    </div>
+  );
+}
+
+function formComponent({
+  elementInstance,
+}: {
+  elementInstance: FormElementInstance;
+}) {
+  const element = elementInstance as CustomInstance;
+  const { label, required, placeHolder, helperText } = element.extraAttributes;
+  return (
+    <div className="flex flex-col gap-2 w-full">
+      <Label>
+        {label}
+        {required && "*"}
+      </Label>
+      <Input placeholder={placeHolder} />
       {helperText && (
         <p className="text-muted-foreground text-[0.8rem]">{helperText}</p>
       )}
